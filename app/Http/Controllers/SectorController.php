6f7f200ddd04e2d9d361e52529bbf717ebf2530d
Ricardo <sricardolimaa@gmail.com>
2026-05-03 13:08:17 -0300

    feat: sector endpoint

---
+++ app/Http/Controllers/SectorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSectorRequest;
+use App\Models\Sector;
+use Illuminate\Http\JsonResponse;
+
+class SectorController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        return response()->json(Sector::orderBy('name')->get());
+    }
+
+    public function store(StoreSectorRequest $request): JsonResponse
+    {
+        $sector = Sector::create($request->validated());
+        return response()->json($sector, 201);
+    }
+
+    public function destroy(Sector $sector): JsonResponse
+    {
+        if ($sector->tickets()->exists()) {
+            return response()->json(['error' => 'Sector has associated tickets.'], 422);
+        }
+
+        $sector->delete();
+        return response()->json(['message' => 'Sector deleted.']);
+    }
+}
